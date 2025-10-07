de595812d4b0ba949752f28c5f625de80c734cef
arijaye <arijaye_derritt@mines.edu>
2025-10-06 20:08:48 -0600

    intialized repo

---
+++ src/BankAccount.java
@@ -0,0 +1,14 @@
+import file_exceptions.NegativeBalanceException;
+public class BankAccount {
+	private double balance;
+	private double withdrawAmount;
+	public BankAccount(double balance){
+		this.balance = balance;
+	}
+	
+	public void withdraw(double withdrawAmount) throws Exception{
+		if (this.balance > this.withdrawAmount) {
+			throw new NegativeBalanceException("Withdraw Amount exceeds current account balance.");
+		}
+	}
+}
---
+++ src/file_exceptions/NegativeBalanceException.java
@@ -0,0 +1,23 @@
+package file_exceptions;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.*;       // for File and PrintWriter
+import java.util.*;     // for Scanner
+
+
+public class NegativeBalanceException extends Exception{
+	double negativeBalance;
+	
+	public NegativeBalanceException() {
+		super("Error: negative balance");
+	}
+	
+	public NegativeBalanceException(double negativeBalance){
+		super("Amount exceeds balance by "+ negativeBalance);
+		this.negativeBalance = negativeBalance;
+		PrintWriter out = new PrintWriter("logfile.txt");
+	}
+	
+	
+}
